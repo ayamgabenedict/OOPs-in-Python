de2aa6427494339ad8fa3b54e2d234a0949a575c
Benedict Ayamga <benedict.ayamga1@gmail.com>
2023-09-12 11:20:16 -0700

    Updated the Polymorphism file

--- polymorphism.py
+++ polymorphism.py
@@ -10,20 +10,18 @@ class University:
 class PostGrad(University):
    def level(self):
     print(f"{self.learn()}", end='')
-    print('Masters or PHd at the postgraduate level')
-
+    print("Masters or PHd at the postgraduate level")
 
 
 class UnderGrad(University):
    def level(self):
     print(f"{self.learn()}", end='')
-    print('Certificate, Diploma or Degree at the undergradute level')
+    print("Certificate, Diploma or Degree at the undergradute level")
 
 
 uni=University()
 undergrad=UnderGrad()
 postgrad=PostGrad()
-
 
 uni.level()
 undergrad.level()
@@ -31,6 +29,19 @@ postgrad.level()
 
 
 
-
+#OOP - Method Overloading
+def add(*args):
+   if (len(args) == 2):
+      result = args[0] + args[1]
+      return result
+   else:
+      result = 0
+      for each_item in args:
+         result += each_item
+      return result
+   
+print(add(10))
+print(add(10, 20))
+print(add(10, 20, 30, 40, 50, 60, 70))
   
 
